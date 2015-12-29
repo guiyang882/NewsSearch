cc2a9dfe7000e326b5fe5a91c3882785396a1848
liuguiyangnwpu <liuguiyangnwpu@163.com>
2015-12-29 12:06:58 -0500

    sorted the key word result !

--- News_Index/index.py
+++ News_Index/index.py
@@ -8,11 +8,13 @@ connection = pymongo.Connection("127.0.0.1",27017)
 db = connection["python"]
 cursor = db["test"]
 
-key_index_map = {}
 
 def load_News_DB():
     contents = cursor.find({},{"news_title":1,"news_key":1,"_id":0})
+    index = 0
+    key_index_map = {}
     for item in contents:
+        index = index + 1
         if item.has_key("news_key"):
             news_key_str = item["news_key"]
             news_key = json.loads(news_key_str)
@@ -22,7 +24,22 @@ def load_News_DB():
                         key_index_map[word].append((item["news_title"],news_key[word]))
                     else:
                         key_index_map[word] = [(item["news_title"],news_key[word])]
-    with open("index.json",'w') as handle:
+        if index % 10000 == 0:
+            with open("index_"+str(index) + ".json",'w') as handle:
+                for tmp in key_index_map.keys():
+                    tmp_list = sorted(key_index_map[tmp],key = lambda x:x[1],reverse = False)
+                    key_index_map[tmp] = []
+                    for title in tmp_list:
+                        key_index_map[tmp].append(title[0])
+                handle.write(json.dumps(key_index_map))
+            key_index_map = {}
+
+    with open("index_"+str(index) + ".json",'w') as handle:
+        for tmp in key_index_map.keys():
+            tmp_list = sorted(key_index_map[tmp],key = lambda x:x[1],reverse = False)
+            key_index_map[tmp] = []
+            for title in tmp_list:
+                key_index_map[tmp].append(title[0])
         handle.write(json.dumps(key_index_map))
 
 if __name__ == "__main__":
